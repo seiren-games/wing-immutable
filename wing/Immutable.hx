c65bc207de6c634957a1f1dcfefdf0e2ea5889e6
seiren <seirengames@gmail.com>
2021-10-17 18:02:09 +0900

    Release

---
+++ wing/Immutable.hx
@@ -0,0 +1,28 @@
+package wing;
+
+import haxe.Exception;
+
+/**
+ * The variable is assigned only once.  
+ * Declare as final or never.  
+ */
+@:forward
+abstract Immutable<T>(Null<T>) to Null<T> {
+	public inline function new(?value:Null<T>) {
+		this = value;
+	}
+
+	/**
+	 * Can only be assigned once if the value is null.
+	 */
+	// Unfortunately, Assignment overloading is not supported
+	// @:op(A = B)
+	public inline function assign(value:T):T {
+		if (this != null) {
+			throw new AlreadyAssignError("Cannot assign `" + value + "`, `" + this + "` is already assigned.");
+		}
+		return this = value;
+	}
+}
+
+class AlreadyAssignError extends Exception {}
